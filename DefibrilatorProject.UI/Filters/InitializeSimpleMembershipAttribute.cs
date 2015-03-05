afefdbf19b1b34559bf036b224e8f1b96f1113e1
Brancinho <branko.bauk@gmail.com>
2015-03-05 23:21:02 +0100

    comm

--- DefibrilatorProject.UI/Filters/InitializeSimpleMembershipAttribute.cs
+++ DefibrilatorProject.UI/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace DefibrilatorProject.UI.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("DefibrilatorProjectContext", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
--- DefibrilatorProject.UI/Models/AccountModels.cs
+++ DefibrilatorProject.UI/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace DefibrilatorProject.UI.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("DefibrilatorProjectContext")
         {
         }
 

d28d761bbe46a2850d0073ff7027e529e8eb711f
Brancinho <branko.bauk@gmail.com>
2015-03-07 23:03:00 +0100

    commit

---
+++ DefibrilatorProject.BusinessLogic/CompanyManager/CompanyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using DefibrilatorProject.DataLayer.Repositories;
+using DefibrilatorProject.Models.Models;
+
+namespace DefibrilatorProject.BusinessLogic.CompanyManager
+{
+    public class CompanyManager
+    {
+        private readonly CompanyRepository _companyRepository = new CompanyRepository();
+        public List<Company> GetCompanies()
+        {
+            return _companyRepository.GetCompanies();
+        }
+
+        public void AddCompany(Company company)
+        {
+            _companyRepository.AddCompany(company);
+        }
+
+        public void EditCompany(int id, Company company)
+        {
+            _companyRepository.EditCompany(id, company);
+        }
+
+        public void DeleteCompany(int id)
+        {
+            _companyRepository.DeleteCompany(id);
+        }
+
+        public Company GetCompany(int id)
+        {
+            return _companyRepository.GetCompany(id);
+        }
+    }
+}

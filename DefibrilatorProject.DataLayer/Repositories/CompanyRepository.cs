1c90bf0e9130dac60ace852803dfde5ebd3a7d4c
Brancinho <branko.bauk@gmail.com>
2015-03-07 23:01:39 +0100

    commit

---
+++ DefibrilatorProject.DataLayer/Repositories/CompanyRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DefibrilatorProject.DataLayer.Context;
+using DefibrilatorProject.Models.Models;
+
+namespace DefibrilatorProject.DataLayer.Repositories
+{
+    public class CompanyRepository
+    {
+        private readonly DefibrilatorProjectContext _db = new DefibrilatorProjectContext();
+
+        public List<Company> GetCompanies()
+        {
+            return _db.Company.ToList();
+        }
+
+        public void AddCompany(Company company)
+        {
+            _db.Company.Add(company);
+            _db.SaveChanges();
+        }
+
+        public void EditCompany(int id, Company company)
+        {
+            var companyToEdit = _db.Company.FirstOrDefault(p => p.CompanyId == id);
+            if (companyToEdit == null) return;
+            companyToEdit.Name = company.Name;
+            _db.SaveChanges();
+        }
+
+        public void DeleteCompany(int id)
+        {
+            var companyToDelete = _db.Company.FirstOrDefault(p => p.CompanyId == id);
+            _db.Company.Remove(companyToDelete);
+            _db.SaveChanges();
+        }
+
+        public Company GetCompany(int id)
+        {
+            return _db.Company.FirstOrDefault(p => p.CompanyId == id);
+        }
+    }
+}

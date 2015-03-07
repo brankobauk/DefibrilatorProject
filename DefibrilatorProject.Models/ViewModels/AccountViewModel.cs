c5714d9bcc10b1530f8b6974979ca153012719e6
Brancinho <branko.bauk@gmail.com>
2015-03-07 22:59:03 +0100

    commit

---
+++ DefibrilatorProject.Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using DefibrilatorProject.Models.Models;
+
+namespace DefibrilatorProject.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        public UserProfile UserProfile { get; set; }
+        public IEnumerable<SelectListItem> Companies { get; set; }
+    }
+}

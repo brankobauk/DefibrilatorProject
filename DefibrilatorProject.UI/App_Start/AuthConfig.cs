3f4ce5b66e4cbc1468fc2dadaf9dead693bb382d
Brancinho <branko.bauk@gmail.com>
2015-03-08 21:09:19 +0100

    commit

--- DefibrilatorProject.UI/App_Start/AuthConfig.cs
+++ DefibrilatorProject.UI/App_Start/AuthConfig.cs
@@ -10,10 +10,9 @@ namespace DefibrilatorProject.UI
 {
     public static class AuthConfig
     {
-        private static readonly AccountManager _accountManager = new AccountManager();
         public static void RegisterAuth()
         {
-            _accountManager.InitializeSimpleMembership();
+            
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 

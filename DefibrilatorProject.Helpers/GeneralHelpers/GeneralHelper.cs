534a6365791d8da6f4567785ccd0daba95f59990
Brancinho <branko.bauk@gmail.com>
2015-03-13 23:15:02 +0100

    date fix

--- DefibrilatorProject.Helpers/GeneralHelpers/GeneralHelper.cs
+++ DefibrilatorProject.Helpers/GeneralHelpers/GeneralHelper.cs
@@ -5,15 +5,19 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace DefibrilatorProject.Helpers.GeneralHelpers
 {
     public class GeneralHelper
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
         public DateTime FormatDate(DateTime date)
         {
+            _logger.Info(date);
             TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
             DateTime utc = TimeZoneInfo.ConvertTimeToUtc(date, tz);
+            _logger.Info(utc);
             return utc;
         }
     }
